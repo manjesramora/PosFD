d3d401bde02a9eb1029248627d04c10ef76aa7e9
Jorge <jorgenoriegarivera@gmail.com>
2024-07-04 09:18:12 -0600

    style: Modificar el formato de la etiqueta en la vista labe.blade
    - Actualizado el formato de la etiqueta en la vista `labe.blade` para mejorar la presentación y alineación.

--- resources/views/label.blade.php
+++ resources/views/label.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Etiqueta de Código de Barras</title>
     <style>
@@ -12,9 +11,7 @@
         .label-container {
             display: flex;
             flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            border: 1px solid #000;
+            justify-content: flex-start;
             padding: 5px;
             box-sizing: border-box;
             width: 6cm;
@@ -23,39 +20,66 @@
             page-break-after: always;
         }
 
+        .description-barcode-container {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            width: 100%;
+        }
+
         .description {
-            text-align: center;
-            font-size: 12px;
+            text-align: left;
+            font-size: 11px;
+            font-weight: bold;
             margin-bottom: 5px;
+            width: 100%;
+            margin-left: 24px; /* Ajusta este valor según sea necesario */
+        }
+
+        .barcode-sku-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            width: 100%;
         }
 
         .barcode {
             display: flex;
             justify-content: center;
-            margin-bottom: 5px;
+            align-items: center;
+            width: 100%;
+        }
+
+        .barcode img, .barcode div {
+            display: block;
+            margin: 0 auto;
         }
 
         .sku {
-            font-size: 10px;
+            font-size: 16px;
             text-align: center;
+            font-weight: bold;
         }
     </style>
 </head>
-
 <body>
     @foreach ($labels as $label)
     <div class="label-container">
-        <div class="description">
-            {{ $label['description'] }}
-        </div>
-        <div class="barcode">
-            {!! $label['barcode'] !!}
-        </div>
-        <div class="sku">
-            SKU: {{ $label['sku'] }}
+        <div class="description-barcode-container">
+            <div class="description">
+                {{ $label['description'] }}
+            </div>
+            <div class="barcode-sku-container">
+                <div class="barcode">
+                    {!! $label['barcode'] !!}
+                </div>
+                <div class="sku">
+                    {{ $label['sku'] }}
+                </div>
+            </div>
         </div>
     </div>
     @endforeach
 </body>
-
 </html>
